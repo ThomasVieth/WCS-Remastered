d072b897d6db7e61f875107001815ac8641ec4de
ThomasVieth <thomas_vieth@hotmail.co.uk>
2021-04-13 10:07:41 +0100

    Clean up of auto-import of extensions.

--- addons/source-python/plugins/warcraft/extensions/__init__.py
+++ addons/source-python/plugins/warcraft/extensions/__init__.py
@@ -10,8 +10,11 @@ from os.path import dirname, basename, isfile
 ## __all__ declaration
 
 modules = glob(dirname(__file__) + "/*")
-__all__ = [
-    basename(f)[:-3] for f in modules if isfile(f)
-] + [
-    basename(f) for f in modules if not isfile(f) and basename(f) != "extensions"
-]
+__all__ = []
+
+for f in modules:
+    f_basename = basename(f)
+    if isfile(f):
+        __all__.append(f_basename[:-3])
+    elif f_basename != "extensions":
+        __all__.append(f_basename)
